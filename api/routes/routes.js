e287fcf232d435a72a85c256d9e8484f79eb1490
BohmChad-FS <crbohm@student.fullsail.edu>
2022-07-17 21:30:09 -0400

    Added jwt, result for login is coming up dundefined, do not know why.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,11 +4,13 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const User = require('../model/user')
 const { findUser, saveUser } = require('../../db/db')
+const checkAuth = require('../../auth/checkAuth')
+const jwt = require('jsonwebtoken')
 
 routes.post('/signup', (req, res) => {
     findUser({email: req.body.email})
     .then(result => {
-        console.log(result)
+        //console.log(result)
         if(result.length > 0) {
             return res.status(409).json({ message: "Email already exists"})
         } else {
@@ -39,6 +41,14 @@ routes.post('/signup', (req, res) => {
             })
         }
     })
+    .catch(err => {
+        console.error(err.message);
+        res.status(500).json({
+            message: {
+                error: err.message
+            }
+        })
+    })
 });
 
 routes.post('/login', (req, res) => {
@@ -47,13 +57,19 @@ routes.post('/login', (req, res) => {
         if(result.length < 1) {
             res.status(401).json({ message: "Email does not exist" })
         } else {
-            bcrypt.compare(req.body.password, req.body.hash, (err, result) => {
+            bcrypt.compare(req.body.password, result.password, (err, result) => {
+                // console.log(res.email)
+                //console.log(result)
+                console.log(req.body.password)
+                console.log(result)
+                const token = jwt.sign({email: req.email, id: User._id}, process.env.jwt_key)
                 if(err) return res.status(501).json({ error: {message: err.message} })
                 if(result) {
                     res.status(200).json({
                         message: "Authentication Successful",
                         result: result,
                         name: req.body.firstName,
+                        token: token
                     })
                 } else {
                     res.status(401).json({ message: "Authentication Failed" })
@@ -63,9 +79,9 @@ routes.post('/login', (req, res) => {
     })
 });
 
-routes.get('/profile', (req, res) => {
+routes.get('/profile', checkAuth, (req, res, next) => {
     res.status(200).json({
-        message: '/profile -GET'
+        message: decoded
     });
 });
 
